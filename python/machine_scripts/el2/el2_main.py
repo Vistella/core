273b327fea8e13d19f565c2b4f88fde8c8c8d2b1
AshkanAD <ashkan.a.dastgerdi@gmail.com>
2021-08-12 09:54:58 +0100

    add el1 changes

--- python/machine_scripts/el2/el2_main.py
+++ python/machine_scripts/el2/el2_main.py
@@ -1,8 +1,10 @@
-from picamera import PiCamera
-from time import sleep
-camera = PiCamera()
-camera.resolution = (1024, 786)#(2592, 1944) 
+from datetime import datetime
+import tkinter as tk
+from tkinter import simpledialog
+from PIL import ImageTk, Image  
+import os
+#from tkinter import messagebox
+from subprocess import Popen
 
-camera.exposure_mode = 'night'
-sleep(2)
-camera.capture("/home/pi/test.jpg")
+
+os.system('raspistill -ss 200000 -sh 100 -ISO 2000 -co 50 -o /home/pi/el2_image.png')
