22d8c5a38c73d6856014a714f72670f0ad231f3a
Maximilian Schinke <maximilianschinke@Sysops-MacBook-Pro.local>
2022-03-04 22:34:58 +0100

    Changed GUI to be full screen

--- python/machine_scripts/layup/stringPanelMatchGUI.py
+++ python/machine_scripts/layup/stringPanelMatchGUI.py
@@ -5,24 +5,41 @@ import time
 
 window = tk.Tk()
 window.title("String to Panel matching")
-frame = tk.Frame(window)
-frame.pack()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window.geometry(str(screen_width)+'x'+str(screen_height))
 
-canvas = tk.Canvas(window, width=450, height=300, bg="white")
-canvas.pack()
+#frame = tk.Frame(window)
+#frame.pack()
 
-oval_panel = canvas.create_oval(10, 10, 30, 30, fill="white")
-canvas.create_window(60, 20, window=tk.Label(window, text= "Panel"))
+canvas = tk.Canvas(window, width=screen_width, height=0.8*screen_height, bg="white")
+canvas.grid(row=3,column=0,columnspan = 5)
+canvas.grid()
+#canvas.pack()
+id_string = []
+id_label = []
+id_string.append(tk.StringVar())
+id_label.append(tk.Label(window, textvariable= id_string[-1], font=("Courier", 44), fg = "red"))
+canvas.create_window(600, 100, window=id_label[-1])
+oval_panel = canvas.create_oval(60, 60, 140, 140, fill="white")
+canvas.create_window(230, 100, window=tk.Label(window, text= "Panel:", font=("Courier", 44)))
 oval_string = []
 for string in range(1,7):
-    oval_string.append(canvas.create_oval(10, 10+30*string, 30, 30+30*string, fill="white"))
-    canvas.create_window(60, 20+30*string, window=tk.Label(window, text= "String " + str(string)))
-dataEntry = tk.Entry(window)
-canvas.create_window(200, 140, window=dataEntry)
+    oval_string.append(canvas.create_oval(60, 60+100*string, 140, 140+100*string, fill="white"))
+    canvas.create_window(270, 100+100*string, window=tk.Label(window, text= "String " + str(string) + ":", font=("Courier", 44)))
+    # Displays the ID
+    
+    id_string.append(tk.StringVar())
+    id_label.append(tk.Label(window, textvariable= id_string[-1], font=("Courier", 44), fg = "red"))
+    canvas.create_window(600, 100+100*string, window=id_label[-1])
+dataEntry = tk.Entry(window,  font=("Courier", 44))
+dataEntry.grid(column=2, row=1) 
+#canvas.create_window(200, 140, window=dataEntry)
 labelText = tk.StringVar()
 labelText.set("Enter Panel ID")
-infoLabel = tk.Label(window, textvariable= labelText)
-canvas.create_window(200, 110, window=infoLabel)
+infoLabel = tk.Label(window, textvariable= labelText, font=("Courier", 44))
+infoLabel.grid(column=2, row=0) 
+#canvas.create_window(200, 110, window=infoLabel)
 
 stringIds = [] #list to store the 6 strings
 panelId = 0
@@ -37,8 +54,10 @@ def scanQR(event):
             stringIds = [] #list to store the 6 strings
 
             canvas.itemconfig(oval_panel, fill="green")
-            for item in oval_string:
+            id_string[0].set(panelId)
+            for x, item in enumerate(oval_string):
                 canvas.itemconfig(item, fill="white")
+                id_string[x+1].set("           ")
             labelText.set("Enter ID for String No. " + str(len(stringIds)+1))
         
         elif (scanInputString[1:], panelId) in stringIds:
@@ -47,9 +66,10 @@ def scanQR(event):
         elif scanInputString[0] == 's' and (scanInputString[1:], panelId) not in stringIds and int(panelId) != 0:    # String QR Codes start with s, i.e. s00012
             stringIds.append((scanInputString[1:], panelId)) #save strings in db without leading 's'
             canvas.itemconfig(oval_string[len(stringIds)-1], fill="green")
+            id_string[len(stringIds)].set(scanInputString)
             labelText.set("Enter ID for String No. " + str(len(stringIds)+1))
         if len(stringIds) == 6:            # If all strings are scanned, upload to DB
-            #Write to DB
+            labelText.set("Uploading....")
             #try:
             conn = psycopg2.connect(user="mjjyypvqfcescn", password="e93dc8ef167aa960b56248e5a2231cbc7d7ad5854266e7df2ab867763f065629", host="ec2-63-34-97-163.eu-west-1.compute.amazonaws.com",port="5432",database="d94t9tih4i30sp")
             cur = conn.cursor()
@@ -75,6 +95,8 @@ def scanQR(event):
                 for item in oval_string:
                     canvas.itemconfig(item, fill="white")
                 time.sleep(1)
+                for item in id_string:
+                    item.set("           ")
             #labelText.set("Enter Panel ID")
 
     dataEntry.delete(0,"end")
