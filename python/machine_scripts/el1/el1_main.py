158136a34a52394a6cdbff2007ddbbc05c719e78
MaxS <schinke.max@gmail.com>
2022-07-16 15:26:53 +0100

    Added inventory change

--- python/machine_scripts/el1/el1_main.py
+++ python/machine_scripts/el1/el1_main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import tkinter as tk
 from tkinter import simpledialog
 from PIL import ImageTk, Image
-from gpiozero import LED
+#from gpiozero import LED
 import os
 import psycopg2
 #from tkinter import messagebox
@@ -13,7 +13,10 @@ from subprocess import Popen
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from matplotlib.pylab import cm
+import pandas as pd
 
+font_type = ("Courier", 20)
 def order_points(pts):
     # initialzie a list of coordinates that will be ordered
     # such that the first entry in the list is the top-left,
@@ -75,29 +78,157 @@ def four_point_transform(image, pts):
     resized = cv2.resize(warped, (400,400), interpolation = cv2.INTER_AREA)
     resized_border = cv2.copyMakeBorder(resized,2,2,2,2,cv2.BORDER_CONSTANT, value = [255,255,255])
     kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
-    im = cv2.filter2D(resized_border, -1, kernel)
     # return the warped image
-    return im
+    return cv2.filter2D(resized_border, -1, kernel)
 
+def read_query_as_df(query):
+    connection = None
+    connection = psycopg2.connect(user="jzztvyjdirgomm", password="974386311e9bf8265574baead65862ee677601c0f8e05bc954785e899d86dfaa", host="ec2-34-247-151-118.eu-west-1.compute.amazonaws.com",port="5432",database="djaki03gmcu3o")
+    cursor = connection.cursor()  
+    temp = []
+    result = None
+    cursor.execute(query)
+    results = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+    connection.close()
+    for result in results:
+        result = list(result)
+        temp.append(result)
+    df = pd.DataFrame(temp, columns=colnames)
+    connection.close
+    return df
 
+#Read possible panels types
+query = """
+SELECT
+pspt.id, 
+pspt.name
+FROM production.solar_panel_type pspt
+"""
+panel_types = read_query_as_df(query)
+print(panel_types)
 
 #Create window with screen size
 window = tk.Tk()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 window.title("Vistella - Electroluminisence Test")
-window.geometry(str(screen_width)+'x'+str(screen_height))
-power = LED(21)
+window.geometry(f'{str(screen_width)}x{str(screen_height)}')
+
+#Add drop_down
+options = tk.StringVar(window)
+options.set(panel_types["name"].iloc[0]) # default value
+optionMenu1 =tk.OptionMenu(window, options, *panel_types["name"])
+optionMenu1.grid(row=1,column=7, sticky='nsew')
+optionMenu1.config(font=font_type)
+menu = window.nametowidget(optionMenu1.menuname)
+menu.config(font=font_type) # set the drop down menu font
+
+#power = LED(21)
 date = 0
 def run_clicked():
+    #Check for material
+    labelText.set("Checking materials")
+    window.update()
+    enoughMaterial = True
+    
+    #Get material requirements for that string    
+    query = """
+        SELECT
+        ppmr.material_id, ppmr.amount/pspt.string_count as amount
+        FROM production.panel_material_req ppmr
+        JOIN production.material pm
+        ON pm.id = ppmr.material_id
+        JOIN production.material_type pmt
+        ON pmt.id = pm.material_type_id
+        JOIN production.solar_panel_type pspt
+        ON pspt.id = ppmr.panel_type_id
+        WHERE pmt.type in ('Tabbing Ribbon', 'Solar Cell')
+        AND ppmr.panel_type_id = %s
+        """%(str(panel_types.index[panel_types['name'] == options.get()].tolist()[0]+1))
+    material_req = read_query_as_df(query)
+    print(material_req)
+    conn = psycopg2.connect(user="jzztvyjdirgomm", password="974386311e9bf8265574baead65862ee677601c0f8e05bc954785e899d86dfaa", host="ec2-34-247-151-118.eu-west-1.compute.amazonaws.com",port="5432",database="djaki03gmcu3o")
+    cur = conn.cursor()
+
+    global queries
+    queries = []
+    string_id = dataEntry.get()
+    for index, line in material_req.iterrows():     
+        query = """
+            SELECT 
+            pic.lot_id, 
+            sum(pic.quantity),
+            min(pl.updated_at) as moved_to_prod
+            FROM production.inventory_changelog pic
+            JOIN production.lot pl
+            ON pic.lot_id = pl.id
+            JOIN finance.purchase_order_line ppol
+            ON ppol.id = pl.purchase_order_line_id
+            WHERE
+                bin_id = 2 AND
+                ppol.material_id = %s
+            GROUP BY
+                pic.lot_id
+        """%line["material_id"]
+        available_lot = read_query_as_df(query)
+        
+        if available_lot.empty:
+            labelText.set('Material', line["material_id"],' never added to production')
+            window.update()
+            print('Material', line["material_id"],'never added to production')
+            enoughMaterial = False
+            print("not enoughMaterial")
+            break
+        if available_lot["sum"].sum() == 0:
+            labelText.set("No more material left")
+            enoughMaterial = False
+            print("not enoughMaterial")
+            break
+        #display(available_lot)
+        for index2, row in available_lot.iterrows():
+            if round(row["sum"],4) == 0: #if lot empty, go to the next lot
+                pass
+            elif row["sum"] >= line["amount"]:
+                labelText.set("Lot has enough material " + str(line["material_id"]))
+                window.update()
+                print("Lot has enough material",line["material_id"] ,"left. Stock: ", round(row["sum"],4), ", needed: ", round(line["amount"],4), ", new stock: ", round(row["sum"] - line["amount"],4))
+                query = """INSERT INTO production.inventory_changelog (quantity, change_type_id, lot_id, panel_id) 
+                VALUES (%s, 10, %s, %s, %s)
+                """
+                queries.append([query, [str(round(-line["amount"],4)), str(row["lot_id"]), str(string_id)]])
+                line["amount"] = 0
+            else:
+                labelText.set("Multiple lots needed for material: " + str(round(line["material_id"],4)))
+                window.update()
+                print("Multiple lots needed for material:",round(line["material_id"],4) ,". Stock: ", round(row["sum"],4), ", needed: ", round(line["amount"],4), ", new stock: 0")
+                #Reduce first lot to 0
+                query = """INSERT INTO production.inventory_changelog (quantity, change_type_id, lot_id, panel_id)
+                VALUES (%s, 10, %s, %s, %s)
+                """
+                queries.append([query, [str(round(-row["sum"],4)), str(row["lot_id"]), str(string_id)]])
+                
+                line["amount"] = round(line["amount"]- row["sum"],4) #Reduce lot by remaining stock
+                
+            if line["amount"] == 0:
+                break
+        if line["amount"] > 0:
+            labelText.set('Not enough Material ' + str(line["material_id"]))
+            window.update()
+            enoughMaterial = False
+            print("not enoughMaterial")
+            break
+
     #Get Date
     global date
     date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    power.on()
+    #power.on()
     #Start Photo taking on EL2 - Check that the file does exist in the right location on EL2
     p = Popen("ssh pi@192.168.8.22 'cd ~ && python3 /home/pi/core/python/machine_scripts/el2/el2_main.py'", shell=True) #Start long lasting command
     # ... do other stuff while subprocess is running
     #Take image on EL1
+    labelText.set('Take images')
+    window.update()
     os.system('raspistill -ss 3000000 -sh 100 -ISO 800 -co 50 -o /home/pi/el1_image.png')
     print('EL1 image taken')
     #Copy from EL2 to EL1
@@ -109,18 +240,19 @@ def run_clicked():
     
     # define the points for the 5 left cells that will be shown on top
     pts_top = []
-    pts_top.append(np.array([(50, 760), (470, 710), (500, 1320), (75, 1290)], dtype = "float32"))
+    pts_top.append(np.array([(40, 760), (470, 710), (500, 1320), (75, 1290)], dtype = "float32"))
     pts_top.append(np.array([(470, 710), (1080, 660), (1100, 1340), (500, 1320)], dtype = "float32"))
-    pts_top.append(np.array([(1080, 660), (1740, 650), (1760, 1310), (1100, 1340)], dtype = "float32"))
-    pts_top.append(np.array([(1730, 650), (2235, 670), (2250, 1240), (1750, 1310)], dtype = "float32"))
-    pts_top.append(np.array([(2235, 670), (2550, 700), (2550, 1190), (2250, 1240)], dtype = "float32"))
-    # define the points for the 5 right cells that will be shown on bottom
+    pts_top.append(np.array([(1070, 650), (1720, 640), (1740, 1310), (1090, 1340)], dtype = "float32"))
+    pts_top.append(np.array([(1710, 640), (2210, 660), (2225, 1230), (1730, 1300)], dtype = "float32"))
+    pts_top.append(np.array([(2210, 670), (2530, 700), (2530, 1190), (2215, 1240)], dtype = "float32"))
+    # define the points for the right cells that will be shown on bottom
     pts_bot = []
     pts_bot.append(np.array([(40, 700), (440, 680), (420, 1240), (25, 1190)], dtype = "float32"))
     pts_bot.append(np.array([(430, 680), (980, 685), (960, 1290), (410, 1240)], dtype = "float32"))
     pts_bot.append(np.array([(980, 685), (1570, 710), (1560, 1300), (960, 1290)], dtype = "float32"))
     pts_bot.append(np.array([(1570, 710), (2060, 750), (2040, 1280), (1560, 1300)], dtype = "float32"))
     pts_bot.append(np.array([(2060, 750), (2390, 780), (2370, 1250), (2040, 1280)], dtype = "float32"))
+
 
     # apply the four point tranform to obtain a "birds eye view" of
     # the images
@@ -145,8 +277,9 @@ def run_clicked():
     for number in range(6,11):
         cv2.putText(combined, str(number), (-200+402*(number-5),900), font,1, (255, 255, 255), 2, cv2.LINE_AA)
         
-    cv2.imwrite('/home/pi/el_images/' + str(date) +'.jpg', combined)
- 
+    
+    colorized = cm.CMRmap(combined)
+    cv2.imwrite('/home/pi/el_images/' + str(date) +'.jpg', colorized*255)
     img = Image.open('/home/pi/el_images/' + str(date) +'.jpg')
     width, height =(img.size) #Get combined dimensions
     img = img.resize((int(screen_width), int(height*(screen_width/width))), Image.ANTIALIAS)
@@ -154,14 +287,16 @@ def run_clicked():
     panel = tk.Label(window, image=img)
     panel.image = img
     panel.grid(column=0, row=4, columnspan=30)
-    text = tk.Label(window, text="Successfully Ran", font=("Courier", 20))
+    text = tk.Label(window, text="Successfully Ran", font=font_type)
     text.grid(column=6, row=1)
     print("Run")
-    power.off()
+    #power.off()
     good_button["state"] = "normal"
     bad_button["state"] = "normal"
     delete_button["state"] = "normal"
-
+    conn.commit()
+    conn.close()
+    
 def good():
     save_clicked("good")
 
@@ -170,7 +305,16 @@ def bad():
 
 def save_clicked(quality):    
     global date
+    global queries
     string_id = dataEntry.get()
+
+    #Write to db:
+    conn = psycopg2.connect(user="jzztvyjdirgomm", password="974386311e9bf8265574baead65862ee677601c0f8e05bc954785e899d86dfaa", host="ec2-34-247-151-118.eu-west-1.compute.amazonaws.com",port="5432",database="djaki03gmcu3o")
+    cur = conn.cursor()
+    for query, data in queries:
+        cur.execute(query, data)
+    conn.commit()
+    conn.close()
 
     #rename image
     file_name = r'/home/pi/el_images/' + str(string_id) + "_" + quality + '.jpg'
@@ -178,7 +322,7 @@ def save_clicked(quality):
     print(file_name)
     print('scp ' + file_name +  ' vistella@167.235.50.52:/var/www/html/img/el_images')
     os.rename(r'/home/pi/el_images/' + str(date) +'.jpg', file_name)
-    text = tk.Label(window, text="String uploading...",font=("Courier", 20))
+    text = tk.Label(window, text="String uploading...",font=font_type)
     text.grid(column=6, row=1)
     conn = psycopg2.connect(user="jzztvyjdirgomm", password="974386311e9bf8265574baead65862ee677601c0f8e05bc954785e899d86dfaa", host="ec2-34-247-151-118.eu-west-1.compute.amazonaws.com",port="5432",database="djaki03gmcu3o")
     cur = conn.cursor()
@@ -194,7 +338,7 @@ def save_clicked(quality):
     os.system('scp ' + file_name +  ' vistella@167.235.50.52:/var/www/html/img/el_images')
     os.remove('/home/pi/el2_image.png')
     
-    text = tk.Label(window, text="String uploaded",  font=("Courier", 20))
+    text = tk.Label(window, text="String uploaded",  font=font_type)
     text.grid(column=6, row=1)
     print("Saved")
     conn.close()
@@ -205,7 +349,7 @@ def save_clicked(quality):
 def delete_clicked():
     global date
     os.remove('/home/pi/el_images/' + str(date) +'.jpg')
-    text = tk.Label(window, text="Image Deleted",  font=("Courier", 20))
+    text = tk.Label(window, text="Image Deleted",  font=font_type)
     text.place(x=570,y=5)
     print("Deleted" + 'el_images/' + str(date) +'.jpg')
     good_button["state"] = "disabled"
@@ -214,14 +358,14 @@ def delete_clicked():
 
 
 #Add buttons
-run_button = tk.Button(window, text="Run EL test", command=run_clicked,  font=("Courier", 20))
-good_button = tk.Button(window, text="Good", command=good,  font=("Courier", 20))
-bad_button = tk.Button(window, text="Bad", command=bad,  font=("Courier", 20))
-delete_button = tk.Button(window, text="Delete Image", command=delete_clicked,  font=("Courier", 20))
-dataEntry = tk.Entry(window,  font=("Courier", 20))
+run_button = tk.Button(window, text="Run EL test", command=run_clicked,  font=font_type)
+good_button = tk.Button(window, text="Good", command=good,  font=font_type)
+bad_button = tk.Button(window, text="Bad", command=bad,  font=font_type)
+delete_button = tk.Button(window, text="Delete Image", command=delete_clicked,  font=font_type)
+dataEntry = tk.Entry(window,  font=font_type)
 labelText = tk.StringVar()
 labelText.set("Enter String ID")
-infoLabel = tk.Label(window, textvariable= labelText,  font=("Courier", 20))
+infoLabel = tk.Label(window, textvariable= labelText,  font=font_type)
 infoLabel.grid(column=0, row=1) 
 dataEntry.grid(column=1, row=1)
 run_button.grid(column=2, row=1) 
@@ -231,13 +375,13 @@ delete_button.grid(column=5, row=1)
 good_button["state"] = "disabled"
 bad_button["state"] = "disabled"
 delete_button["state"] = "disabled"
-img = Image.open('/home/pi/logo.png')
-width, height =(img.size) #Get combined dimensions
-img = img.resize((int(screen_width), int(height*(screen_width/width))), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(img)
-panel = tk.Label(window, image=img)
-panel.image = img
-panel.grid(column=0, row=4, columnspan=30)       
+#img = Image.open('/home/pi/logo.png')
+#width, height =(img.size) #Get combined dimensions
+#img = img.resize((int(screen_width), int(height*(screen_width/width))), Image.ANTIALIAS)
+#img = ImageTk.PhotoImage(img)
+#panel = tk.Label(window, image=img)
+#panel.image = img
+#panel.grid(column=0, row=4, columnspan=30)       
 window.bind("<Enter>", run_clicked)
 window.mainloop()
 
